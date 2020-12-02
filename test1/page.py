8ef6e14cb20c3b6afbbb513fd79cf68d1f5877f3
Andy <yrranadres@gmail.com>
2020-12-01 16:46:24 -0800

    Home User Login automation complete

---
+++ test1/page.py
@@ -0,0 +1,52 @@
+from locators import *
+
+class BasePage(object):
+    url = None
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def navigate(self):
+        self.driver.get(self.url)
+
+class SelectUserPage(BasePage):
+    url = "https://clarity.dexcom.com/"
+
+    def title_found(self):
+        return "Dexcom CLARITY" in self.driver.title
+
+    def homeUserButtonFound(self):
+        return self.driver.find_elements(*SelectUserPageLocators.USER_BUTTON)
+
+    def clickHomeUserButton(self):
+        element = self.homeUserButtonFound()
+        if len(element) > 0:
+            element[0].click()
+
+class AccountPage(BasePage):
+    def title_found(self):
+        return "Dexcom - Account Management" in self.driver.title
+
+    def usernameInputBoxFound(self):
+        return self.driver.find_elements(*AccountPageLocators.USERNAME_BOX)
+
+    def passwordInputBoxFound(self):
+        return self.driver.find_elements(*AccountPageLocators.PASS_BOX)
+
+    def loginButtonFound(self):
+        return self.driver.find_elements(*AccountPageLocators.LOGIN_BUTTON)
+
+    def typeUsernameInput(self):
+        element = self.usernameInputBoxFound()
+        if len(element) > 0:
+            element[0].send_keys("codechallenge")
+
+    def typePasswordInput(self):
+        element = self.passwordInputBoxFound()
+        if len(element) > 0:
+            element[0].send_keys("Password123")
+
+    def clickLoginButton(self):
+        element = self.loginButtonFound()
+        if len(element) > 0:
+            element[0].click()
