6462161bd83ffc0cf00169eaab9203307ea3ba7a
Andy <yrranadres@gmail.com>
2020-12-02 08:19:52 -0800

    removed test1 folder

--- test1/locators.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class SelectUserPageLocators(object):
-    USER_BUTTON = (By.XPATH,'//a[text()="Dexcom CLARITY for Home Users"]')
-
-class AccountPageLocators(object):
-    USERNAME_BOX = (By.ID,'username')
-    PASS_BOX = (By.ID,'password')
-    LOGIN_BUTTON = (By.XPATH,'//input[@value="Login" and @name="op"]')
